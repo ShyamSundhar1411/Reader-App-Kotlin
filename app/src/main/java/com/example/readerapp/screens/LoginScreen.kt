7d3a7425aa9e23ca89e2312efb93e9e4e0a16b69
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-06-29 15:22:47 +0530

    feat: added animation to splash screen

--- app/src/main/java/com/example/readerapp/screens/LoginScreen.kt
+++ app/src/main/java/com/example/readerapp/screens/LoginScreen.kt
@@ -1,9 +1,11 @@
 package com.example.readerapp.screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun LoginScreen(navController: NavController){
+    Text(text = "Login Screen")
 
 }
--- app/src/main/java/com/example/readerapp/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/example/readerapp/screens/ReaderSplashScreen.kt
@@ -1,10 +1,79 @@
 package com.example.readerapp.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.Animatable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.readerapp.navigation.Routes
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.ui.draw.scale
+import kotlinx.coroutines.delay
 
 @Composable
 fun ReaderSplashScreen(navController: NavController){
-    Text(text = "SplashScreen")
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true){
+
+        scale.animateTo(
+            targetValue = 0.9f,
+            animationSpec = tween(
+                durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(8f).getInterpolation(it)
+                }
+        ))
+        delay(2000L)
+        navController.navigate(Routes.LoginScreen.name)
+    }
+    Surface(
+        modifier = Modifier.padding(15.dp).size(330.dp).scale(scale.value),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(
+            width = 2.dp,
+            color = Color.LightGray
+        ),
+
+    ) {
+        Column(
+            modifier = Modifier.padding(100.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Text(
+                text = "Reader",
+                style = MaterialTheme.typography.headlineLarge,
+                color = Color.Red.copy(0.5f)
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(text = "\"Read. Change. Yourself\"",
+                style = MaterialTheme.typography.headlineSmall,
+                color = Color.Black.copy(0.5f),
+                modifier = Modifier.padding(
+                    bottom = 100.dp
+                )
+            )
+        }
+
+    }
 }
