33649563e37848f9a367d742f786478fe74d35fc
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2024-07-03 23:56:42 +0530

    feat: added home content

---
+++ app/src/main/java/com/example/readerapp/models/MBook.kt
@@ -0,0 +1,5 @@
+package com.example.readerapp.models
+
+data class MBook(var id:String? = null,
+                 var title:String? = null,
+    )
--- app/src/main/java/com/example/readerapp/screens/HomeScreen.kt
+++ app/src/main/java/com/example/readerapp/screens/HomeScreen.kt
@@ -1,14 +1,18 @@
 package com.example.readerapp.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.readerapp.components.FABComponent
 import com.example.readerapp.components.ReaderTopBarComponent
+import com.example.readerapp.models.MBook
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
@@ -22,4 +26,16 @@ fun HomeScreen(navController: NavHostController) {
     ) {
         Box(modifier = Modifier.padding(it))
     }
+}
+
+@Composable
+fun ReadingRightNowArea(books: List<MBook>){
+    Surface(modifier = Modifier.padding(start = 5.dp,top = 1.dp)){
+        Column{
+            Text(text = "Your reading activity right now"
+            )
+
+        }
+    }
+
 }
